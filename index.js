d4f7891a4c61aaad3507cfbe48b4ff1b7833724f
Nick Vinson <7684172+NMVW@users.noreply.github.com>
2022-12-06 19:38:46 +0000

    update source of api server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /**
 DO NOT TOUCH THIS FILE
 */
+
+// API hosted in a repl.it repo https://github.com/Company-Six/fs-dev-interview-backend
 const BASE_API_URL = 'https://API-Service.nickvinson1.repl.co';
 const axios = require('axios');
 
